e17c772909d248012be5fcbcc97abeb43891e60f
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-23 18:08:37 -0300

    criando as rotas do meetapp

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import LoginController from './app/controllers/LoginController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/login', LoginController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
