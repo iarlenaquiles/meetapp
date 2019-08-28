350901b489169e6ff33cc628447f02ad4b570f57
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 02:49:38 -0300

    criando controller do meetup

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,11 @@
+class MeetupController {
+  async index(req, res) {}
+
+  async store(req, res) {}
+
+  async update(req, res) {}
+
+  async delete(req, res) {}
+}
+
+export default new MeetupController();
