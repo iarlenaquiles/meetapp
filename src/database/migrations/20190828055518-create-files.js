fe3ecc0998725ec406a1b42696841c4286c74252
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 03:01:08 -0300

    criando migration files

---
+++ src/database/migrations/20190828055518-create-files.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('files');
+  },
+};
