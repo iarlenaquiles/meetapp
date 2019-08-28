ec145a83c8421e81b391f910c910978ebb26ddeb
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 02:53:31 -0300

    carregando models

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,13 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Meetup from '../app/models/Meetup';
+import Subscription from '../app/models/Subscription';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Meetup, Subscription];
 
 class Database {
   constructor() {
