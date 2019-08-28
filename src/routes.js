20f41beddd9b10799706238ab189e8d556d58cf6
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 03:15:13 -0300

    carregando config do multer

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import LoginController from './app/controllers/LoginController';
 import FileController from './app/controllers/FileController';
@@ -6,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/login', LoginController.store);
