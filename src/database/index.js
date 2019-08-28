1f4b775e92da10201e8eb3a0d99a0867a47b8f4f
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 03:12:33 -0300

    carregando associates

--- src/database/index.js
+++ src/database/index.js
@@ -12,12 +12,21 @@ const models = [User, File, Meetup, Subscription];
 class Database {
   constructor() {
     this.init();
+    this.associate();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach(model => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
   }
 }
 
