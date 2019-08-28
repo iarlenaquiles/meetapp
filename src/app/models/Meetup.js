963967401d0f885d76243e68132b436042c89623
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 02:49:59 -0300

    criando model do meetup

---
+++ src/app/models/Meetup.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+import { isBefore } from 'date-fns';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'file_id' });
+    this.belongsTo(models.User, { foreignKey: 'user_id' });
+    this.hasMany(models.Subscription, { foreignKey: 'meetup_id' });
+  }
+}
+
+export default Meetup;
