61ee4a90bb65ffb2a61323c3c6a7abb9f1103387
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-28 23:40:42 -0300

    ajustando nome da const

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -3,7 +3,7 @@ import File from '../models/File';
 class FileController {
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
-    const File = await File.create({
+    const file = await File.create({
       name,
       path,
     });
