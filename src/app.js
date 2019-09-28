04f6a2cb55a70a8134272a09e9cd0ba68d3793bb
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-27 21:53:09 -0300

    config cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import './database';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -25,6 +26,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(express.urlencoded({ extended: false }));
     this.server.use(
       '/files',
